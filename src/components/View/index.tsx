d2132c01c8cb9eb884b9117afb0d98cf7bb1a55b
Marvin Petate <46154422+petatemarvin26@users.noreply.github.com>
2023-06-20 20:28:58 +0800

    release@v1.0.6 (#27)
    * Fix View Component
    
    * Fix ForwardRef Of View Component
    
    * release@v1.0.6

--- src/components/View/index.tsx
+++ src/components/View/index.tsx
@@ -4,28 +4,20 @@ import styles from './.module.css';
 
 class View extends React.PureComponent<Props, States> {
   render(): React.ReactNode {
-    const {id, className, style, viewRef, children, onClick} = this.props;
+    const Component = forwardRef<HTMLDivElement, Props>((props, ref) => {
+      const {className, children} = this.props;
 
-    let _className = styles['view'];
-    if (className) _className = ` ${className}`;
+      let _className = styles['view'];
+      if (className) _className = ` ${className}`;
 
-    return (
-      <div
-        id={id}
-        ref={viewRef}
-        className={_className}
-        style={style}
-        onClick={onClick}
-      >
-        {children}
-      </div>
-    );
+      return (
+        <div {...props} className={_className} ref={ref}>
+          {children}
+        </div>
+      );
+    });
+    return <Component {...this.props} />;
   }
 }
 
-const forwardView: React.ForwardRefRenderFunction<View, Props> = (
-  props,
-  ref
-) => <View viewRef={ref} {...props} />;
-
-export default forwardRef<View, Props>(forwardView);
+export default View;
